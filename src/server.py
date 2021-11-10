fa46b2482c3f29ed2153c5608d458097dad2ff1c
Stanley77-web <timothystanleysetiawan555@gmail.com>
2021-11-10 17:33:28 +0700

    Nyicil Laporan

--- src/server.py
+++ src/server.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, compression_folder ='imagecompression')
 
 @app.route("/")
 def main():
-    return render_template()
+    return render_template('imgcompression.py')
 
 if __name__ == "__main__":
     app.run()
